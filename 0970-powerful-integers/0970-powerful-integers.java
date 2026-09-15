4c503d110b69200dec46a53ec651106f0baad8db
JosephPrasanthReddy <josephprasanthreddy@gmail.com>
2026-09-15 12:54:32 +0530

    Time: 1 ms (100%), Space: 42.5 MB (93.44%) - LeetHub

---
+++ 0970-powerful-integers/0970-powerful-integers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        HashSet<Integer> a=new HashSet<>();
+        for(int i=1;i<=bound;i*=x)
+        {
+            for(int j=1;j<=bound;j*=y)
+            {
+                if(i+j<=bound)
+                {
+                    a.add(i+j);
+                }
+                if(y==1)
+                {
+                    break;
+                }
+            }
+            if(x==1)
+            {
+                break;
+            }
+        }
+        return new ArrayList<>(a);
+    }
+}
