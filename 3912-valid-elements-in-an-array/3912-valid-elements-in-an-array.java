8cff0990d37eae3448a5e776f37bc7ddefdf5ded
JosephPrasanthReddy <josephprasanthreddy@gmail.com>
2026-09-23 12:42:31 +0530

    Time: 1 ms (100%), Space: 46.5 MB (81.88%) - LeetHub

---
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findValidElements(int[] nums) {
+
+        int n = nums.length;
+        int[] right = new int[n];
+
+        right[n-1] = nums[n-1];
+
+        for(int i=n-2;i>=0;i--) {
+            right[i] = Math.max(right[i+1], nums[i]);
+        }
+
+        int left = 0;
+
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i=0;i<n;i++) {
+
+            if(i == 0 || i == n-1 ||
+               nums[i] > left ||
+               nums[i] > right[i+1]) {
+                ans.add(nums[i]);
+            }
+
+            left = Math.max(left, nums[i]);
+        }
+
+        return ans;
+    }
+}
